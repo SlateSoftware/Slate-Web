8d13eecd0b1c25ff37cab5ac9d8418d0b8b7c437
Jacob <dukkcc@proton.me>
2025-09-27 07:17:38 -0700

    Start fresh: reset slate-web for new build

--- apps/web-os/src/components/windows/TestWindow.tsx
+++
@@ -1,7 +0,0 @@
-export function TestWindow() {
-  return (
-    <div className="p-6 bg-slate-950 h-full w-full">
-      <p className="text-xs text-green-200">A pretty awesome test window</p>
-    </div>
-  );
-}
