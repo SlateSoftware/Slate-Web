a15d5567140ef7ace918f1277768d21f280bf9a3
Jacob <dukkcc@proton.me>
2025-09-28 09:03:26 -0700

    setup prettier eslint stuff

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,10 @@
-import { defineConfig } from 'eslint-define-config'
+import { defineConfig } from 'eslint/config'
+import js from '@eslint/js'
 import svelte from 'eslint-plugin-svelte'
 import astro from 'eslint-plugin-astro'
 
 export default defineConfig([
-  {
-    files: ['**/*.js', '**/*.ts'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    extends: ['eslint:recommended', 'prettier'],
-  },
-  {
-    files: ['**/*.svelte'],
-    plugins: { svelte },
-    extends: ['plugin:svelte/recommended', 'prettier'],
-  },
-  {
-    files: ['**/*.astro'],
-    plugins: { astro },
-    extends: ['plugin:astro/recommended', 'prettier'],
-  },
+    js.configs.recommended,
+    ...svelte.configs.recommended,
+    ...astro.configs.recommended,
 ])
