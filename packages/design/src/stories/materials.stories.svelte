a45cf110b8779078cabff720c2339c5579d41018
dukcc <dukkcc@proton.me>
2025-10-22 16:27:39 -0700

    finish materials

--- packages/design/src/stories/materials.stories.svelte
+++ packages/design/src/stories/materials.stories.svelte
@@ -29,20 +29,126 @@
                     <p>FG Base Subtle</p>
                 </div>
             </div>
-            <div
-                class="bg-surface-primary-success text-text-success-contrast shadow-glass rounded-8 border-stroke w-full border p-4 pt-16"
-            >
-                <p>surface-primary-success</p>
+            <div class="flex gap-2">
+                <div
+                    class="bg-fg-inset shadow-glass rounded-8 border-stroke w-full border p-4 pt-16"
+                >
+                    <p>FG Inset</p>
+                </div>
+                <div
+                    class="bg-fg-inset-p shadow-glass rounded-8 border-stroke-p w-full border p-4 pt-16"
+                >
+                    <p>FG Inset Pronounced</p>
+                </div>
+                <div
+                    class="bg-fg-inset-s shadow-glass rounded-8 border-stroke-s w-full border p-4 pt-16"
+                >
+                    <p>FG Inset Subtle</p>
+                </div>
             </div>
-            <div
-                class="bg-surface-primary-warning text-text-warning-contrast shadow-glass rounded-8 border-stroke w-full border p-4 pt-16"
-            >
-                <p>surface-primary-warning</p>
+
+            <div class="flex gap-2">
+                <div class="bg-fg-red shadow-glass rounded-8 border-stroke w-full border p-4 pt-16">
+                    <p>FG Red</p>
+                </div>
+                <div class="bg-fg-red-p shadow-glass rounded-8 border-red w-full border p-4 pt-16">
+                    <p>FG Red Pronounced</p>
+                </div>
+                <div
+                    class="bg-fg-red-s shadow-glass rounded-8 border-stroke-s w-full border p-4 pt-16"
+                >
+                    <p>FG Red Subtle</p>
+                </div>
             </div>
-            <div
-                class="bg-surface-primary-danger text-text-danger-contrast shadow-glass rounded-8 border-stroke w-full border p-4 pt-16"
-            >
-                <p>surface-primary-danger</p>
+
+            <div class="flex gap-2">
+                <div
+                    class="bg-fg-orange shadow-glass rounded-8 border-stroke w-full border p-4 pt-16"
+                >
+                    <p>FG Orange</p>
+                </div>
+                <div
+                    class="bg-fg-orange-p shadow-glass rounded-8 border-orange w-full border p-4 pt-16"
+                >
+                    <p>FG Orange Pronounced</p>
+                </div>
+                <div
+                    class="bg-fg-orange-s shadow-glass rounded-8 border-stroke-s w-full border p-4 pt-16"
+                >
+                    <p>FG Orange Subtle</p>
+                </div>
+            </div>
+
+            <div class="flex gap-2">
+                <div
+                    class="bg-fg-yellow shadow-glass rounded-8 border-stroke w-full border p-4 pt-16"
+                >
+                    <p>FG Yellow</p>
+                </div>
+                <div
+                    class="bg-fg-yellow-p shadow-glass rounded-8 border-yellow w-full border p-4 pt-16"
+                >
+                    <p>FG Yellow Pronounced</p>
+                </div>
+                <div
+                    class="bg-fg-yellow-s shadow-glass rounded-8 border-stroke-s w-full border p-4 pt-16"
+                >
+                    <p>FG Yellow Subtle</p>
+                </div>
+            </div>
+
+            <div class="flex gap-2">
+                <div
+                    class="bg-fg-green shadow-glass rounded-8 border-stroke w-full border p-4 pt-16"
+                >
+                    <p>FG Green</p>
+                </div>
+                <div
+                    class="bg-fg-green-p shadow-glass rounded-8 border-green w-full border p-4 pt-16"
+                >
+                    <p>FG Green Pronounced</p>
+                </div>
+                <div
+                    class="bg-fg-green-s shadow-glass rounded-8 border-stroke-s w-full border p-4 pt-16"
+                >
+                    <p>FG Green Subtle</p>
+                </div>
+            </div>
+
+            <div class="flex gap-2">
+                <div
+                    class="bg-fg-blue shadow-glass rounded-8 border-stroke w-full border p-4 pt-16"
+                >
+                    <p>FG Blue</p>
+                </div>
+                <div
+                    class="bg-fg-blue-p shadow-glass rounded-8 border-blue w-full border p-4 pt-16"
+                >
+                    <p>FG Blue Pronounced</p>
+                </div>
+                <div
+                    class="bg-fg-blue-s shadow-glass rounded-8 border-stroke-s w-full border p-4 pt-16"
+                >
+                    <p>FG Blue Subtle</p>
+                </div>
+            </div>
+
+            <div class="flex gap-2">
+                <div
+                    class="bg-fg-purple shadow-glass rounded-8 border-stroke w-full border p-4 pt-16"
+                >
+                    <p>FG Purple</p>
+                </div>
+                <div
+                    class="bg-fg-purple-p shadow-glass rounded-8 border-purple w-full border p-4 pt-16"
+                >
+                    <p>FG Purple Pronounced</p>
+                </div>
+                <div
+                    class="bg-fg-purple-s shadow-glass rounded-8 border-stroke-s w-full border p-4 pt-16"
+                >
+                    <p>FG Purple Subtle</p>
+                </div>
             </div>
         </div>
     </div>
