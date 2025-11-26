6747922412d0ff954c3f3432d8ad28b6928820a8
Jacob <97273940+dukcc@users.noreply.github.com>
2025-11-26 09:43:13 -0800

    add label story to checkbox story

--- packages/design/src/stories/checkbox.stories.svelte
+++ packages/design/src/stories/checkbox.stories.svelte
@@ -17,11 +17,15 @@
             checked: {
                 control: { type: "boolean" },
             },
+            label: {
+                control: { type: "text" },
+            },
         },
         args: {
             indeterminate: false,
             disabled: false,
             checked: true,
+            label: "Agree to terms and conditions",
             onCheckedChange: fn(),
         },
         parameters: {
@@ -38,4 +42,14 @@
 </script>
 
 <Story name="Checkbox" let:args />
+
+{#snippet templateLabel(args)}
+    <div class="flex items-center gap-6">
+        <Checkbox id="checkbox" {...args} /><label for="checkbox" class="text-body text-text"
+            >{args.label}</label
+        >
+    </div>
+{/snippet}
+<Story name="Label" let:args template={templateLabel} />
+
 <Story name="Invalid" args={{ "aria-invalid": true }} let:args />
